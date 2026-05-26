40a24fef045f6ad07be8fbb0d7b23a2dd1feb2ab
Uroojdev <83166791+Uroojdev@users.noreply.github.com>
2026-05-26 13:59:15 +0530

    Bump i18n cache key to v3

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -14,7 +14,7 @@ i18n
     detection: {
       order: ['localStorage', 'navigator'],
       caches: ['localStorage'],
-      lookupLocalStorage: 'pv_lang_v2',
+      lookupLocalStorage: 'pv_lang_v3',
     },
     interpolation: { escapeValue: false },
   });
