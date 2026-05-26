f7692b4580c88f695f973ea5952ef256ec631cf4
Uroojdev <83166791+Uroojdev@users.noreply.github.com>
2026-05-27 00:03:16 +0530

    Fix Brand.jsx hook and tagline translation

--- src/components/auth/ui/Brand.jsx
+++ src/components/auth/ui/Brand.jsx
@@ -1,14 +1,15 @@
 import { useTranslation } from 'react-i18next';
 import { Logo } from './Logo';
-
-export const Brand = () => (
-  <div className="pv-brand">
-    <div className="pv-lbox"><Logo/></div>
-    <div>
-      <div className="pv-ltext">Pedi<em>Vault</em></div>
-      <div className="pv-lsub">{t('common.tagline','Child Health Records')}</div>
+export const Brand = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="pv-brand">
+      <div className="pv-lbox"><Logo/></div>
+      <div>
+        <div className="pv-ltext">Pedi<em>Vault</em></div>
+        <div className="pv-lsub">{t('common.tagline','Child Health Records')}</div>
+      </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default Brand;
